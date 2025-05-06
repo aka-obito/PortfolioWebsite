d5a5ef9468b97b5680485e26274930ad5db4fcda
Dhawal Turkar <dhawalturkar@gmail.com>
2025-05-06 15:00:13 +0530

    Update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,70 +1,70 @@
 import React from "react";
 import { motion } from "framer-motion";
 import "./Projects.css";
-import pr1 from "../assets/pr1.jpg";
-import pr2 from "../assets/pr2.jpg";
-import pr3 from "../assets/pr3.jpg";
-import pr4 from "../assets/pr4.jpg";
-import pr5 from "../assets/pr5.jpg";
-import alarmVideo from "../assets/AlarmVideo.mp4";
-import pr7 from "../assets/pr7.png";
-import pr8 from "../assets/pr8.jpg";
-import pr9 from "../assets/pr9.png";
+import pr1 from "../assets/video/pr1.mp4";
+import pr2 from "../assets/video/pr2.mp4";
+import pr3 from "../assets/video/pr3.mp4";
+import pr4 from "../assets/video/pr4.mp4";
+import pr5 from "../assets/video/pr5.mp4";
+import pr6 from "../assets/video/pr6.mp4";
+import pr7 from "../assets/video/pr7.mp4";
+import pr8 from "../assets/video/pr8.mp4";
+import pr9 from "../assets/video/pr9.mp4";
 
 const projects = [
   { 
     title: "Minor Project", 
     description: "Sign Language Recognition Model Using CNN ", 
     githubLink: "https://github.com/aka-obito/VQAModel",
-    img: pr1
+    video: pr1
   },
   { 
     title: "Major Project", 
     description: "Deep Learning LSTM Model For Dynamic Sign Gestures.", 
     githubLink: "https://github.com/aka-obito/SignLanguageMajorProject",
-    img: pr2
+    video: pr2
   },
   { 
     title: "Capstone Project", 
     description: "KRUSHI MITRA: Crop Yield Prediction Using Machine Learning", 
     githubLink: "https://github.com/yourusername/flask-file-upload",
-    img: pr3
+    video: pr3
   },
   { 
     title: "EchoBeats Music", 
     description: "Robust Music App for Music streaming And Chatting With Friends.", 
     githubLink: "https://github.com/yourusername/object-detection",
-    img: pr4
+    video: pr4
   },
   { 
     title: "VQA Model", 
     description: "Deep Learning Approch for Visual Quetion Answering.", 
     githubLink: "https://github.com/yourusername/stock-price-predictor",
-    img: pr5
+    video: pr5
   },
   {
     title: "Anti-Theft Alarm Monitoring System",
     description: "A full-stack e-commerce platform with payment integration.",
     githubLink: "https://github.com/yourusername/ecommerce-website",
-    video: alarmVideo
+    video: pr6
   },  
   { 
-    title: "Portfolio Website", 
+    title: "Online Personlised Learning Platform", 
     description: "A React.js personal portfolio with a dynamic project showcase.", 
     githubLink: "https://github.com/yourusername/portfolio-website",
-    img: pr7
+    video: pr7
   },
   { 
     title: "AI Proctoring System", 
     description: "A web-based chat application using WebSockets and Node.js.", 
     githubLink: "https://github.com/yourusername/real-time-chat-app",
-    img: pr8
+    video: pr8
   },
   { 
     title: "Weather App", 
     description: "A web-based chat application using WebSockets and Node.js.", 
     githubLink: "https://github.com/yourusername/real-time-chat-app",
-    img: pr9
+    video: pr9
   }
 ];
 
